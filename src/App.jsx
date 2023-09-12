75558b06d6d9a82be8c6c467d18ddf33fa85445b
chioma <chiomachristopher91@gmail.com>
2023-09-11 17:52:36 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <div className="container">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
